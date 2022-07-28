d50d3e168dc89e3f44f694d7d89a3c4ae46d2f36
fvmunoz <fvmunoz@gmail.com>
2022-07-28 12:58:06 -0400

    more on results analysis... next commit with include even more testing

--- analize.py
+++ analize.py
@@ -10,9 +10,11 @@ import argparse
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
   parser.add_argument("inputfile", type=str, help="input file")
-  parser.add_argument('--size', '-s', type=int, help="size in nearest neighbors"
+  parser.add_argument('--size', '-n', type=int, help="size in nearest neighbors"
                       " of the cluster to build", default=0)
+  parser.add_argument('--smooth', '-s', action='store_true')
   parser.add_argument('-v', '--verbose', action='store_true')
+  
   
   args = parser.parse_args()
   
@@ -31,7 +33,6 @@ if __name__ == '__main__':
                               marked=Defects.all_defects)
 
   cluster.extend_clusters(args.size)
-
   # cluster.smooth_edges()
   cluster.write(args.inputfile+'_cluster.vasp')
   # cluster.hydrogenate(args.inputfile+'_cluster.vasp')
--- test/run.sh
+++ test/run.sh
@@ -7,5 +7,20 @@ do
     mv ${i}_defect.vasp ${i}_cluster.vasp aux
     diff -q aux/${i}_defect.vasp results/${i}_defect.vasp
     diff -q aux/${i}_cluster.vasp results/${i}_cluster.vasp
+
+    # testing the larger clusters
+    ../analize.py $i -n 1 > temp-n1
+    mv ${i}_cluster.vasp aux/${i}_cluster-n1.vasp
+    rm ${i}_defect.vasp 
+    diff  aux/${i}_cluster-n1.vasp results/${i}_cluster-n1.vasp
+
+    # testing even larger clusters
+    ../analize.py $i -n 2 > temp-n2
+    mv ${i}_cluster.vasp aux/${i}_cluster-n2.vasp
+    rm ${i}_defect.vasp 
+    diff  aux/${i}_cluster-n2.vasp results/${i}_cluster-n2.vasp
+
 done
 diff temp results/results
+diff temp-n1 results/results-n1
+diff temp-n2 results/results-n2
