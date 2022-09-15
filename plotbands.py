28bcdfdaf127a53b766e8a096c9ba22f8d405752
nicolas <nicolasvas@gmail.com>
2022-09-15 20:55:08 -0300

    added SPIN and KPOINT detection to bandsplot, updated TODO list

--- plotbands.py
+++ plotbands.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+import re 
 import poscar
 #import latticeUtils
 import defects
@@ -25,8 +25,6 @@ def plot(orbitals, POSCAR = "POSCAR", PROCAR = "PROCAR",OUTCAR = "OUTCAR", savef
     file.write("##Asumed default values are \n")
     file.write("#Energy limit values \n")
     file.write("elimit = [-2,2] \n")
-    file.write("#Mode of ploting for bands, if the orbitals are not degenerate might want to change to 'plain' \n")
-    file.write("mode = 'parametric' \n")
     file.write("#Orbital dictionary used \n")
     file.write("ORB_Dict  = {'s' :  [0], 'p':[1,2,3], 'd':[4,5,6,7,8], 'f':[9,10,11,12,13,14,15]}\n")
     file.write("#POSCAR,PROCAR, OUTCAR, and Ploted figure files \n")
@@ -42,6 +40,10 @@ def plot(orbitals, POSCAR = "POSCAR", PROCAR = "PROCAR",OUTCAR = "OUTCAR", savef
     POSCAR_.parse()
     Defects = defects.FindDefect(POSCAR_)
     atoms  = Defects.all_defects
+    #If no defects were found, default to use all atoms instead
+    if(atoms == []):
+        print("No defects were found on ", POSCAR)
+        atoms = range(POSCAR_.Ntotal)
     line = "#The defects found on "+POSCAR+" were \n"
     file.write(line)
     file.write("atoms = [".rstrip("\n"))
@@ -53,7 +55,7 @@ def plot(orbitals, POSCAR = "POSCAR", PROCAR = "PROCAR",OUTCAR = "OUTCAR", savef
 
 
     asked_orb = []
-    #Dictionary of orbitals
+    #Picking and writing Orbitals
     for i in orbitals:
         asked_orb += ORB_Dict[i]
     file.write("#Orbitals used for this plot\n")
@@ -64,11 +66,36 @@ def plot(orbitals, POSCAR = "POSCAR", PROCAR = "PROCAR",OUTCAR = "OUTCAR", savef
             file.write(",".rstrip("\n"))
             file.write(str(i).rstrip("\n"))
     file.write("] \n")
+
+    #Checking OUTCAR for SPIN and Kpoint information
+    out = open("OUTCAR" ,"r")
+    out = out.readlines()
+    if(out):
+        match_KPOINTS = [re.findall(r'NKPTS\s*=\s*(\d*)', line) for line in out]
+        match_SPIN = [re.findall(r'ISPIN\s*=\s*(\d*)', line) for line in out]
+        KPOINTS = int([x for x in match_KPOINTS if(x)][0][0])
+        ISPIN = int([x for x in match_SPIN if(x)][0][0])
+        if(ISPIN == 2):
+            file.write("cmap = 'seismic'\n")
+            file.write("vmax = 1.0\n")
+            file.write("vmin = -1.0\n")
+        else:
+            file.write("cmap = 'jet'\n")
+            file.write("vmax = None\n")
+            file.write("vmin = None\n")            
+        if(KPOINTS != 1):
+            file.write("mode = 'parametric'\n")
+        else:
+            file.write("mode = 'atomic'\n")
+    else:
+        print("Couldn't find OUTCAR file, assuming default mode and cmap")
+        file.write("cmap = 'jet'\n")
+        file.write("mode = 'parametric'\n")
+
     file.write("#We then plot the bands specified \n")
-    file.write("pyprocar.bandsplot(PROCAR,outcar = OUTCAR,elimit = elimit,mode = mode,savefig = savefigure,atoms = atoms,orbitals = orbitals)")
+    file.write("pyprocar.bandsplot(PROCAR,outcar = OUTCAR,elimit = elimit,mode = mode,savefig = savefigure,atoms = atoms,orbitals = orbitals, cmap = cmap, vmax = vmax, vmin = vmin)")
     file.close()
-    os.system("chmod +rwx plot_file.py")
-    
+    os.system("chmod +rwx plot_file.py") 
     #pyprocar.bandsplot(PROCAR,outcar = outcar, elimit = elimit, mode = mode_,savefig = savefigure ,atoms = atoms, orbitals = asked_orb)
 
 if __name__ == '__main__':
